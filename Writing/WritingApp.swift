013f54cc354603e41ff540ac32c89160e7de5f1e
Omar Torres Rios <omar.torres@rappi.com>
2023-01-23 23:00:28 -0500

    Initial Commit

---
+++ Writing/WritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingApp.swift
+//  Writing
+//
+//  Created by Omar Torres on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
