84e3c05e12a8f0c70b3fdc7f9eb10a0ed437154e
Omar Torres Rios <omar.torres@rappi.com>
2023-02-28 21:16:20 +0000

    Create NoteView to show the details of the selected note.

---
+++ Writing/NoteView.swift
@@ -0,0 +1,32 @@
+//
+//  Note.swift
+//  Writing
+//
+//  Created by Omar Torres on 20/02/23.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    @Binding var note: Note
+    
+    var body: some View {
+        VStack(spacing: 2.0) {
+            TextField("Note title", text: $note.title, axis: .vertical)
+                .padding([.leading, .trailing], 4)
+            
+            TextEditor(text: $note.body)
+                .foregroundColor(.gray)
+                .font(.body)
+        }
+        .padding()
+    }
+}
+
+struct Note_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteView(note: .constant(Note(id: 1,
+                                      title: "Some title",
+                                      body: "Some body")))
+    }
+}
